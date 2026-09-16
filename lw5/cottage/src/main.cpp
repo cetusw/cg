9132c8645db0fb2b37bda534848d7b143861e33c
cetusw <kugelev.m@gmail.com>
2026-09-16 18:32:23 +0300

    cottage добавил отображение куба

--- lw5/cottage/src/main.cpp
+++ lw5/cottage/src/main.cpp
@@ -5,12 +5,93 @@
 
 #include <iostream>
 
+void setupProjection(const int width, int height)
+{
+    if (height == 0)
+    {
+        height = 1;
+    }
+
+    const double aspect =
+            static_cast<double>(width) /
+            static_cast<double>(height);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    gluPerspective(
+        60.0,
+        aspect,
+        0.1,
+        100.0
+    );
+
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void drawCube()
+{
+    glBegin(GL_QUADS);
+
+    // Передняя грань
+    glColor3f(1.0f, 0.0f, 0.0f);
+
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+
+    // Задняя грань
+    glColor3f(0.0f, 1.0f, 0.0f);
+
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+
+    // Левая грань
+    glColor3f(0.0f, 0.0f, 1.0f);
+
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+
+    // Правая грань
+    glColor3f(1.0f, 1.0f, 0.0f);
+
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+
+    // Верхняя грань
+    glColor3f(1.0f, 0.0f, 1.0f);
+
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+
+    // Нижняя грань
+    glColor3f(0.0f, 1.0f, 1.0f);
+
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+
+    glEnd();
+}
+
 void framebufferSizeCallback(
-    GLFWwindow*,
+    GLFWwindow *,
     const int width,
     const int height)
 {
     glViewport(0, 0, width, height);
+
+    setupProjection(width, height);
 }
 
 int main()
@@ -24,7 +105,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 
-    GLFWwindow* window = glfwCreateWindow(
+    GLFWwindow *window = glfwCreateWindow(
         800,
         600,
         "Cottage",
@@ -40,6 +121,17 @@ int main()
     }
 
     glfwMakeContextCurrent(window);
+
+    int width;
+    int height;
+
+    glfwGetFramebufferSize(window, &width, &height);
+
+    framebufferSizeCallback(
+        window,
+        width,
+        height
+    );
 
     glewExperimental = GL_TRUE;
 
@@ -70,6 +162,17 @@ int main()
             GL_COLOR_BUFFER_BIT |
             GL_DEPTH_BUFFER_BIT
         );
+
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
+        gluLookAt(
+            4.0, -6.0, 4.0,
+            0.0, 0.0, 0.0,
+            0.0, 0.0, 1.0
+        );
+
+        drawCube();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
